b4b6dbbfa939b66c4ce8349b73ff6bcd304b9e5a
Hamzi Roslan <No public email provided>
2023-08-27 20:00:15 +0800

    Changes made from Source Academy

--- Mission2.js
+++ Mission2.js
@@ -1,17 +1,54 @@
-import {make_cross, show, rcross,beside,stack} from "rune";
+import {beside, make_cross, rcross, stack, show, stackn, square, blank, stack_frac,quarter_turn_right} from "rune";
 
+//Q1
 function fractal(pic, n) {
     // your answer here
-    return cross_iter(pic,n,1);
-}
-
-function cross_iter(pic, n, count){
-    return count === n //count stops when === n
+    return n === 1
     ? pic
-    : beside(pic, cross_iter(stack(pic,pic), n, count + 1)); 
+    : fractal(beside(make_cross(rcross), stackn(2, pic)), n - 1);
+}
+  
+  
+show(fractal(make_cross(rcross), 5));  
+
+
+//Q2
+function hook(frac) {
+    // your answer here
+    return frac === 0
+    ? square
+    : stack(square,quarter_turn_right(stack_frac(frac, square, blank)));
+    
+    
     
 }
 
-show(fractal(make_cross(rcross), 3));
-show(fractal(make_cross(rcross), 5));
+// Test
+
+show(hook(1));
+show(hook(1/2));
+show(hook(1/5));
+//
+// Test
+
+//q3
+
+
+function spiral(thickness,depth){
+    return depth === 0
+    ? blank
+    : stack_frac(
+        thickness, 
+        hook(thickness/2), 
+        quarter_turn_right(spiral(thickness, depth - 1)));
+    
+    
+    //stack_frac(thickness, hook(thickness/2), quarter_turn_right(stack_frac(thickness, hook(thickness/2), blank)));
+}
+
+show(spiral(1/5, 2));
+show(spiral(1 / 5, 3));
+show(spiral(1 / 5, 20));
+
+
 
